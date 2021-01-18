80819f571e462871f1b76bf1927c7bf9571ebf71
Дмитрий Шкатов <Dmitriy.Shkatov@waveaccess.ru>
2021-01-18 15:16:31 +0500

    [NF] Creatr table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,11 @@
+create table exchange_value(
+   id INT NOT NULL, 
+   currency_from VARCHAR(50) NOT NULL, 
+   currency_to VARCHAR(20) NOT NULL, 
+   conversion_multiple FLOAT,
+   port INT
+);
+
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
 values(10001,'USD','INR',65,0);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
